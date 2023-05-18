b53a43c9f9fa1b567f972e266de21e6018230d57
Andres Villalobos <andres.villalobos@datagrove.com>
2023-05-18 12:00:50 -0600

    Update main.go

--- main/main.go
+++ main/main.go
@@ -5,7 +5,26 @@ import (
 	"log"
 	"net/http"
 )
-
+func hellohandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello"{
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET"{
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return 
+	}
+	fmt.Printf(w, "hello!")
+}
 func main() {
-	fmt.Println("Hello")
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", hellohandler)
+
+
+	fmt.Printf("Starting server at port 8080")
+	if err:= http.ListenAndServe(":8080", nil); err !=nil {
+		log.Fatal(err)
+	}
 }
